653f9718070b13d39c1dd09092bb581f30974948
moobs2525 <micbon01@easv365.dk>
2026-09-08 11:15:42 +0200

    opstart

---
+++ Shop.Domain/Item.cs
@@ -0,0 +1,15 @@
+﻿namespace Shop.Domain;
+
+public class Item
+{
+    public int Id { get; }
+    public string Description { get; }
+    public decimal Price { get; }
+    
+    public Item(int id, string description, decimal price)
+    {
+        Id = id;
+        Description = description;
+        Price = price;
+    }
+}
